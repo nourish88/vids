fe78395573385cdd231b46440c7e342e64510f18
jugadev <jugadev@outlook.com>
2021-08-04 18:17:19 +0300

    first

---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state={term:''}
+
+
+onFormSubmit=  event=>{
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+}
+
+  render() {
+    return (
+      <div className=" search-bar ui segment">
+        <form  onSubmit={this.onFormSubmit} className="ui form">
+            <div className="field">
+                <label>Video Search</label>
+          <input  type="text"
+          value={this.state.term} onChange={(e)=>this.setState({term:e.target.value})}/>  
+          {/* <input  type="text" onchange={(e)=> console.log(e.target.value)}/> */}
+          </div> 
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
